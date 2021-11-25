d5df3b461ae4fe9c44cbe534c0c060ee19174294
Jeremy De Vera <201980075@psu.palawan.edu.ph>
2021-11-26 00:20:24 +0800

    first commit

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', '');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6454037');
+	define('DB_PASS', 'JQqAZd2mmr');
+	define('DB_NAME', 'sql6454037');
